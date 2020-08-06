ffc10818da9795624efc334ec22cc70669fef112
gsirakavit <gsirakavit@hotmail.com>
2020-08-06 17:21:59 -0500

    replace HttpResponse with render in all views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import Http404
 from .models import Question
 
 def index(request):
@@ -10,11 +10,25 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}.")
+    try:
+        question = Question.objects.get(pk=question_id)
+        question_id = question.id
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/results.html', {'question_id': question_id})
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    try:
+        question = Question.objects.get(pk=question_id)
+        question_id = question.id
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/vote.html', {'question_id': question_id})
